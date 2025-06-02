c1df8cb0e51611acc4833f937325cbd7f5f41232
Martin Portych <portyma1@uhk.cz>
2025-06-02 22:03:41 +0100

    choose from map working

--- app/src/main/java/com/example/weatherapp/fragments/map/MapPickerFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/map/MapPickerFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.fragments.map
 
 import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,44 +27,59 @@ class MapPickerFragment : Fragment(), OnMapReadyCallback {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d("MapPickerFragment", "onCreateView called")
         return inflater.inflate(R.layout.fragment_map_picker, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("MapPickerFragment", "onViewCreated started")
+
         geocoder = Geocoder(requireContext(), Locale.getDefault())
 
         val mapFragment = SupportMapFragment.newInstance()
+        Log.d("MapPickerFragment", "Map fragment instance created")
+
         childFragmentManager
             .beginTransaction()
             .replace(R.id.map_container, mapFragment)
             .commit()
+        Log.d("MapPickerFragment", "Map fragment transaction committed")
 
         mapFragment.getMapAsync(this)
+        Log.d("MapPickerFragment", "getMapAsync called")
     }
 
-
     override fun onMapReady(map: GoogleMap) {
+        Log.d("MapPickerFragment", "onMapReady called")
         googleMap = map
 
         val defaultLocation = LatLng(50.0755, 14.4378) // Prague
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 6f))
+        Log.d("MapPickerFragment", "Camera moved to default location")
 
         googleMap.setOnMapClickListener { latLng ->
+            Log.d("MapPickerFragment", "Map clicked at: ${latLng.latitude}, ${latLng.longitude}")
             googleMap.clear()
             googleMap.addMarker(MarkerOptions().position(latLng))
 
-            val addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
-            val locationName = addressList?.firstOrNull()?.getAddressLine(0) ?: "Vybraná poloha"
+            try {
+                val addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+                val locationName = addressList?.firstOrNull()?.getAddressLine(0) ?: "Vybraná poloha"
+                Log.d("MapPickerFragment", "Location name resolved: $locationName")
 
-            val result = Bundle().apply {
-                putString("locationText", locationName)
-                putDouble("latitude", latLng.latitude)
-                putDouble("longitude", latLng.longitude)
+                val result = Bundle().apply {
+                    putString("locationText", locationName)
+                    putDouble("latitude", latLng.latitude)
+                    putDouble("longitude", latLng.longitude)
+                }
+
+                parentFragmentManager.setFragmentResult("manualLocationSearch", result)
+                findNavController().popBackStack()
+                Log.d("MapPickerFragment", "Result set and popped back")
+            } catch (e: Exception) {
+                Log.e("MapPickerFragment", "Error resolving address: ${e.message}", e)
             }
-
-            parentFragmentManager.setFragmentResult("manualLocationSearch", result)
-            findNavController().popBackStack()
         }
     }
 }
